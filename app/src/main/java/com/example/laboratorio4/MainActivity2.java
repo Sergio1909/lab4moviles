8c61feac6b94460b4294f58ae1ac153939469709
JulioIPv5 <julio.huamani@pucp.edu.pe>
2020-07-09 07:16:09 -0500

    Intento de extraer fotos desde firebase, codigo incompleto

--- app/src/main/java/com/example/laboratorio4/MainActivity2.java
+++ app/src/main/java/com/example/laboratorio4/MainActivity2.java
@@ -1,17 +1,31 @@
 package com.example.laboratorio4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private int[] images = {R.drawable.goku, R.drawable.helicoptero, R.drawable.pucp};
+    //private int[] images = {R.drawable.goku, R.drawable.helicoptero, R.drawable.pucp};
     private RecyclerAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference databaseReference;
+    private List<Upload> Uploads;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +35,31 @@ public class MainActivity2 extends AppCompatActivity {
             layoutManager = new GridLayoutManager(this,2);
             recyclerView.setHasFixedSize(true);
             recyclerView.setLayoutManager(layoutManager);
-            adapter = new RecyclerAdapter(images);
+
+            Uploads = new ArrayList<>();
+            databaseReference = FirebaseDatabase.getInstance().getReference("Uploads");
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                        Upload upload = postSnapshot.getValue(Upload.class);
+                        Uploads.add(upload);
+
+
+
+                    }
+                    adapter = new RecyclerAdapter(MainActivity2.this,);
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(MainActivity2.this,databaseError.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+
             recyclerView.setAdapter(adapter);
 
     }
